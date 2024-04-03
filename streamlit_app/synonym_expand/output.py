254f7e2e299b82076cff32d4a51ed096201dc1e1
SeyeongHan <h960213@gmail.com>
2024-04-03 10:03:25 -0500

    fix: move to src directory

--- streamlit_app/synonym_expand/output.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-
-class Output(BaseModel):
-    synonyms: List[str] = Field(description="Synonyms of keywords provided, make every letter lowercase except for the first letter")
