59d081bedaffd307325bd4f0604275b9b1e5536f
SeyeongHan <h960213@gmail.com>
2024-04-03 10:02:08 -0500

    fix: move to src directory

---
+++ src/synonym_expand/output.py
@@ -0,0 +1,7 @@
+from typing import List
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class Output(BaseModel):
+    synonyms: List[str] = Field(description="Synonyms of keywords provided, make every letter lowercase except for the first letter")
