938f3defa894482ae042aac38bd7c1249f213529
kevinl424 <kevin.li.20742@gmail.com>
2024-04-04 19:07:09 -0500

    add: preliminary benchmark skeleton

--- recursive_retrieval/benchmarking/benchmark.py
+++ recursive_retrieval/benchmarking/benchmark.py
@@ -1,4 +1,19 @@
+import os
+from collections import defaultdict
+
+from dotenv import load_dotenv
+from llama_index.core.llms import ChatMessage
+from llama_index.llms.perplexity import Perplexity
+from pandas import DataFrame
+
 from judge import custom_response_evaluator
+
+load_dotenv()
+pplx_api_key = os.getenv("PERPLEXITY_API_KEY")
+
+query_llm = Perplexity(
+    api_key=pplx_api_key, model="sonar-small-online", temperature=0.5
+)
 
 data_points = [
     "harry potter",
@@ -10,8 +25,28 @@ data_points = [
     "crypto",
     "ut austin",
     "llms",
+    "lions",
 ]
 
+
 def benchmark():
-    base_prompt =  "tell me about"
-    
+    base_prompt = "tell me about "
+    results = defaultdict(list)
+
+    for subject in data_points:
+        query = base_prompt + subject
+        messages_dict = [
+            {"role": "system", "content": "Be precise and concise."},
+            {"role": "user", "content": query},
+        ]
+        messages = [ChatMessage(**msg) for msg in messages_dict]
+        response = query_llm.chat(messages)
+        result = custom_response_evaluator(query, response)
+
+        for score in result:
+            results[subject].append(result[score])
+
+    print(results)
+
+
+benchmark()
--- recursive_retrieval/benchmarking/judge.py
+++ recursive_retrieval/benchmarking/judge.py
@@ -8,8 +8,9 @@ from langchain_openai import OpenAI
 
 from output import Output
 
+# TODO: Try inserting into the template some examples of queries and responses that user would personally give scores of x, y, z to.
 
-def custom_response_evaluator(question: str, response: str) -> Dict[str, int]:
+def custom_response_evaluator(query: str, response: str) -> Dict[str, int]:
     load_dotenv()
     llm = OpenAI(openai_api_key=os.getenv("OPENAI_KEY"), temperature=0)
     parser = JsonOutputParser(pydantic_object=Output)
@@ -18,22 +19,22 @@ def custom_response_evaluator(question: str, response: str) -> Dict[str, int]:
     You are an expert response judger. Score the provided respose a score from 0 to 1000 with 0 being the worst and 1000 being the best.
     Give separate scores for conciseness, informativeness and accuracy.
     For conciceness, focus on how succinct the response is and how clearly the information is conveyed using minimal words.
-    For informativeness, focus on how comprehensive and relevant the contents of the response is to the given question.
+    For informativeness, focus on how comprehensive and relevant the contents of the response is to the given query.
     For accuracy, focus on the correctness and decree of preciseness that the response has.
     Hold the scores you give to the highest quality with higher numbers given only to the best of the best.
     
     Format: {format_instructions}
 
-    Question: {question}
+    Query: {query}
     Response: {response}
     """
 
     prompt = PromptTemplate(
         template=template,
-        input_variables=["question", "response"],
+        input_variables=["query", "response"],
         partial_variables={"format_instructions": parser.get_format_instructions()},
     )
 
     chain = prompt | llm | parser
-    result = chain.invoke({"question": question, "response": response})
+    result = chain.invoke({"query": query, "response": response})
     return result
