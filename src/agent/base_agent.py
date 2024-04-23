e525f3b54967d56a999e4c84763752b2e4da6599
kevinl424 <kevin.li.20742@gmail.com>
2024-04-23 16:11:56 -0500

    fix: integration of routing agent into base_agent

--- src/agent/base_agent.py
+++ src/agent/base_agent.py
@@ -11,60 +11,84 @@ from llama_index.graph_stores.neo4j import Neo4jGraphStore
 from llama_index.core.llms import ChatMessage
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.perplexity import Perplexity
+from llama_index.core.multi_modal_llms.generic_utils import load_image_urls
+from llama_index.core.tools import FunctionTool
+
 
 from src.synonym_expand.synonym import custom_synonym_expand_fn
 from src.memory import MemoryStream, EntityKnowledgeStore
 from src.agent.data_types import Message
 from src.agent.llm_api.tools import openai_chat_completions_request
+from dotenv import load_dotenv
+from llama_index.multi_modal_llms.openai import OpenAIMultiModal
+import geocoder
+from llama_index.core.agent import ReActAgent
+import googlemaps
+
 
 MAX_ENTITIES_FROM_KG = 5
 ENTITY_EXCEPTIONS = ['Unknown relation']
 
 
 class Agent(object):
-    """Agent manages the RAG model, the knowledge graph, and the memory stream."""
+    """Agent manages the RAG model, the ReAct agent, and the memory stream."""
 
     def __init__(self, name, memory_stream_json, entity_knowledge_store_json,
                  system_persona_txt, user_persona_txt, past_chat_json):
-        self.name = name
-        # OpenAI API key
-        self.openai_api_key = os.environ["OPENAI_API_KEY"]
-        self.model_endpoint = 'https://api.openai.com/v1'
+        load_dotenv()
+        # getting necessary API keys
+        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+        googlemaps_api_key = os.getenv("GOOGLEMAPS_API_KEY")
+        pplx_api_key = os.getenv("PERPLEXITY_API_KEY")
+
         # Neo4j credentials
         self.neo4j_username = "neo4j"
         self.neo4j_password = os.getenv("NEO4J_PW")
         self.neo4j_url = os.getenv("NEO4J_URL")
         database = "neo4j"
-        # Perplexity API key
-        pplx_api_key = os.getenv('PERPLEXITY_API_KEY')
 
-        self.llm = OpenAI(temperature=0, model="gpt-3.5-turbo")
-        self.query_llm = Perplexity(api_key=pplx_api_key,
-                                    model="mistral-7b-instruct",
-                                    temperature=0.5)
-        Settings.llm = self.llm
+        # initialize APIs
+        self.openai_mm_llm = OpenAIMultiModal(
+            model="gpt-4-vision-preview",
+            api_key=os.getenv("OPENAI_KEY"),
+            max_new_tokens=300,
+        )
+        llm = OpenAI(model="gpt-3.5-turbo-instruct")
+        self.query_llm = Perplexity(
+            api_key=pplx_api_key, model="mistral-7b-instruct", temperature=0.5
+        )
+        self.gmaps = googlemaps.Client(key=googlemaps_api_key)
+        Settings.llm = llm
         Settings.chunk_size = 512
 
-        self.graph_store = Neo4jGraphStore(
+        # initialize Neo4j graph resources
+        graph_store = Neo4jGraphStore(
             username=self.neo4j_username,
             password=self.neo4j_password,
             url=self.neo4j_url,
             database=database,
         )
 
-        self.storage_context = StorageContext.from_defaults(
-            graph_store=self.graph_store)
-
-        self.graph_rag_retriever = KnowledgeGraphRAGRetriever(
+        self.storage_context = StorageContext.from_defaults(graph_store=graph_store)
+        graph_rag_retriever = KnowledgeGraphRAGRetriever(
             storage_context=self.storage_context,
             verbose=True,
-            llm=self.llm,
+            llm=llm,
             retriever_mode="keyword",
-            with_nl2graphquery=True,
             synonym_expand_fn=custom_synonym_expand_fn,
         )
+
         self.query_engine = RetrieverQueryEngine.from_args(
-            self.graph_rag_retriever, )
+            graph_rag_retriever,
+        )
+
+        search_tool = FunctionTool.from_defaults(fn=self.search)
+        locate_tool = FunctionTool.from_defaults(fn=self.locate)
+        vision_tool = FunctionTool.from_defaults(fn=self.vision)
+
+        self.routing_agent = ReActAgent.from_tools(
+            [search_tool, locate_tool, vision_tool], llm=llm, verbose=True
+        )
 
         self.memory_stream = MemoryStream(memory_stream_json)
         self.entity_knowledge_store = EntityKnowledgeStore(
@@ -72,10 +96,65 @@ class Agent(object):
 
         self.message = Message(system_persona_txt, user_persona_txt,
                                past_chat_json)
+        
 
     def __str__(self):
         return f"Agent {self.name}"
 
+    def external_query(self, query: str):
+            messages_dict = [
+                {"role": "system", "content": "Be precise and concise."},
+                {"role": "user", "content": query},
+            ]
+            messages = [ChatMessage(**msg) for msg in messages_dict]
+            external_response = self.query_llm.chat(messages)
+
+            return str(external_response)
+
+    def search(self, query: str) -> str:
+        """Search the knowledge graph or perform search on the web if information is not present in the knowledge graph"""
+        response = self.query_engine.query(query)
+
+        if response.metadata is None:
+            return self.external_query(query)
+        else:
+            return response
+
+    def locate(self, query: str) -> str:
+        """Finds the current geographical location"""
+        location = geocoder.ip("me")
+        lattitude, longitude = location.latlng[0], location.latlng[1]
+
+        reverse_geocode_result = self.gmaps.reverse_geocode((lattitude, longitude))
+        formatted_address = reverse_geocode_result[0]["formatted_address"]
+        return "Your address is" + formatted_address
+
+    def vision(self, query: str, img_url: str) -> str:
+        """Uses computer vision to process the image specified by the image url and answers the question based on the CV results"""
+        img_docs = load_image_urls([img_url])
+        response = self.openai_mm_llm.complete(prompt=query, image_documents=img_docs)
+        return response
+
+    def query(self, query: str) -> str:
+        # get the response from react agent
+        response = self.routing_agent.chat(query)
+        self.routing_agent.reset()
+        # write response to file for KG writeback
+        with open('data/external_response.txt', 'w') as f:
+            print(response, file=f)
+        return response
+    
+    def write_back(self):
+        documents = SimpleDirectoryReader(
+            input_files=["data/external_response.txt"]
+        ).load_data()
+
+        KnowledgeGraphIndex.from_documents(
+            documents,
+            storage_context=self.storage_context,
+            max_triplets_per_chunk=8,
+        )
+    
     def check_KG(self, query: str) -> bool:
         """Check if the query is in the knowledge graph.
 
@@ -90,50 +169,6 @@ class Agent(object):
         if response.metadata is None:
             return False
         return True
-
-    def external_query(self, query: str):
-        # 1) must query the web
-        messages_dict = [
-            {
-                "role": "system",
-                "content": "Be precise and concise."
-            },
-            {
-                "role": "user",
-                "content": query
-            },
-        ]
-        messages = [ChatMessage(**msg) for msg in messages_dict]
-
-        external_response = self.query_llm.chat(messages)
-
-        # 2) answer needs to be stored into txt file for loading into KG
-        with open('data/external_response.txt', 'w') as f:
-            print(external_response, file=f)
-        return str(external_response)
-
-    def load_KG(self):
-        # TODO: index should be updated with the new data
-        documents = SimpleDirectoryReader(
-            input_files=['data/external_response.txt']).load_data()
-
-        index = KnowledgeGraphIndex.from_documents(
-            documents,
-            storage_context=self.storage_context,
-            max_triplets_per_chunk=5,
-        )
-
-    def add_chapter(self, paths):
-        # TODO: index should be updated with the new data
-        documents = SimpleDirectoryReader(
-            input_files=paths  # paths is list of chapters
-        ).load_data()
-
-        index = KnowledgeGraphIndex.from_documents(
-            documents,
-            storage_context=self.storage_context,
-            max_triplets_per_chunk=2,
-        )
 
     def _change_llm_message_chatgpt(self) -> dict:
         """Change the llm_message to chatgpt format.
@@ -174,16 +209,17 @@ class Agent(object):
         """
         llm_message_chatgpt = self._change_llm_message_chatgpt()
         response = openai_chat_completions_request(self.model_endpoint,
-                                                   self.openai_api_key,
-                                                   llm_message_chatgpt)
+                                                self.openai_api_key,
+                                                llm_message_chatgpt)
         response = str(response['choices'][0]['message']['content'])
         return response
 
-    def get_rag_response(self, query, return_entity=False):
-        """Get response from the RAG model."""
-        response = self.query_engine.query(query)
+    def get_routing_agent_response(self, query, return_entity=False):
+        """Get response from the ReAct."""
+        response = self.query(query)
 
-        if return_entity:
+        if return_entity and self.check_KG(query):
+            # check if request wants entities, and if routing agent agent is able to use KG
             return response, self.get_entity(self.query_engine.retrieve(query))
         return response
 
